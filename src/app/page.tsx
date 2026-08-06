38ac580138068d66ea6ee7b811d3f22692a14751
alexandramarinas <alexandra_marinas@icloud.com>
2026-08-06 22:44:06 +0300

    Rebuild luxury homepage structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,31 +3,23 @@ import Hero from "./components/Hero";
 import Services from "./components/Services";
 import Gallery from "./components/Gallery";
 import About from "./components/About";
-import Facial from "./components/Facial";
 import Testimonials from "./components/Testimonials";
-import FAQ from "./components/FAQ";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import WhatsAppButton from "./components/WhatsAppButton";
 
 export default function Home() {
   return (
     <>
       <Navbar />
-
       <main>
         <Hero />
         <Services />
         <Gallery />
         <About />
-        <Facial />
         <Testimonials />
-        <FAQ />
         <Contact />
       </main>
-
       <Footer />
-      <WhatsAppButton />
     </>
   );
 }
